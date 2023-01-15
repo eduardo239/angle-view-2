f36f135924782bd3458a59e100392dbfaaf3783f
Eduardo Silva <51725601+eduardo239@users.noreply.github.com>
2023-01-15 17:39:11 -0300

    Delete _test directory

--- _test/buttons4.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-root = tk.Tk()
-test = tk.Label(root, text="Red", bg="red", fg="white")
-test.pack(ipadx=30, ipady=6)
-test = tk.Label(root, text="Purple", bg="purple", fg="white")
-test.pack(ipadx=8, ipady=12)
-tk.mainloop()
--- _test/executable.py
+++
@@ -1,8 +0,0 @@
-import random
-
-p = random.randint(0, 100)
-print(p)
-
-# pyinstaller main.py --onefile
-
-s = input()
